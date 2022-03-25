f22446a864fd132f4947940c87665d7d200687bf
tiagofredk <tiagofredk@gmail.com>
2022-03-25 17:21:34 +0100

    undefined

--- src/components/data/Inputdate.js
+++ src/components/data/Inputdate.js
@@ -1,67 +1,47 @@
-import React, { useState, useContext } from 'react';
-import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
-import DatePicker from 'react-native-datepicker';
-import { MainContext } from '../context/MainContextProvider';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-const Inputdate = () => {
-    const {date, setDate} = useContext(MainContext);
-
+export const Inputdate = () => {
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+  
+    const onChange = (event, selectedDate) => {
+      const currentDate = selectedDate;
+      setShow(false);
+      setDate(currentDate);
+    };
+  
+    const showMode = (currentMode) => {
+      setShow(true);
+      setMode(currentMode);
+    };
+  
+    const showDatepicker = () => {
+      showMode('date');
+    };
+  
+    const showTimepicker = () => {
+      showMode('time');
+    };
+  
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.container}>
-                <DatePicker
-                    style={styles.datePickerStyle}
-                    date={date} // Initial date from state
-                    mode="date" // The enum of date, datetime and time
-                    placeholder="select date"
-                    format="DD-MM-YYYY"
-                    minDate="01-01-1800"
-                    maxDate="01-01-2030"
-                    confirmBtnText="Confirm"
-                    cancelBtnText="Cancel"
-                    customStyles={{
-                        dateIcon: {
-                            //display: 'none',
-                            position: 'absolute',
-                            left: 0,
-                            top: 4,
-                            marginLeft: 0,
-                        },
-                        dateInput: {
-                            marginLeft: 36,
-                        },
-                    }}
-                    onDateChange={(date) => {
-                        setDate(date);
-                    }}
-                />
-            </View>
-        </SafeAreaView>
+      <View>
+        <View>
+          <Button onPress={showDatepicker} title="Show date picker!" />
+        </View>
+        <View>
+          <Button onPress={showTimepicker} title="Show time picker!" />
+        </View>
+        <Text>selected: {date.toLocaleString()}</Text>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            onChange={onChange}
+          />
+        )}
+      </View>
     );
-};
-
-export default Inputdate;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    title: {
-        textAlign: 'center',
-        fontSize: 20,
-        fontWeight: 'bold',
-        padding: 20,
-    },
-    datePickerStyle: {
-        width: 200,
-        marginTop: 20,
-    },
-});
+  }
