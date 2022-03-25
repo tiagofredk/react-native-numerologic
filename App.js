592121e8ba28b0a8d43b14e3988a781dab096fa4
tiagofredk <tiagofredk@gmail.com>
2022-03-25 09:25:10 +0100

    stage

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <MainContextProvider>
-
+        
         <StatusBar style="auto" />
       </MainContextProvider>
     </NavigationContainer>
